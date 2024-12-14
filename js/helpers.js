57c314482cdd81e0d2481902e95eaceec02d0905
Ola <seunvigo@gmail.com>
2024-12-14 15:51:25 -0600

    chore: code format

--- js/helpers.js
+++ js/helpers.js
@@ -1,46 +1,46 @@
 function shuffle(array) {
-	let currentIndex = array.length,
-		randomIndex
+  let currentIndex = array.length,
+    randomIndex
 
-	// While there remain elements to shuffle.
-	while (currentIndex != 0) {
-		// Pick a remaining element.
-		randomIndex = Math.floor(Math.random() * currentIndex)
-		currentIndex--
+  // While there remain elements to shuffle.
+  while (currentIndex != 0) {
+    // Pick a remaining element.
+    randomIndex = Math.floor(Math.random() * currentIndex)
+    currentIndex--
 
-		// And swap it with the current element.
-		;[array[currentIndex], array[randomIndex]] = [
-			array[randomIndex],
-			array[currentIndex],
-		]
-	}
+      // And swap it with the current element.
+      ;[array[currentIndex], array[randomIndex]] = [
+        array[randomIndex],
+        array[currentIndex],
+      ]
+  }
 
-	return array
+  return array
 }
 
 function randomInt(min = 0, max = 255) {
-	// min and max included
-	return Math.floor(Math.random() * (max - min + 1) + min)
+  // min and max included
+  return Math.floor(Math.random() * (max - min + 1) + min)
 }
 
 function convertRGB(color, toNum = false) {
-	let item = color
+  let item = color
 
-	if (typeof color !== "string") {
-		item = color.toString()
-	}
+  if (typeof color !== "string") {
+    item = color.toString()
+  }
 
-	return item.split(",").map((number) => {
-		const formattedNum = Number(number)
-		if (toNum) {
-			return formattedNum / 255
-		}
-		return formattedNum * 255
-	})
+  return item.split(",").map((number) => {
+    const formattedNum = Number(number)
+    if (toNum) {
+      return formattedNum / 255
+    }
+    return formattedNum * 255
+  })
 }
 
 export {
-    convertRGB,
-    randomInt,
-    shuffle,
+  convertRGB,
+  randomInt,
+  shuffle,
 }
