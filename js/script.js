e63f86639d578ffb40bf9d01748219d460b640f8
Ola <seunvigo@gmail.com>
2024-12-14 15:46:36 -0600

    chore: script code format

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-import AI from './AI.js';
+import AI from "./AI.js"
 import { convertRGB, shuffle, randomInt } from "./helpers.js"
 import { tertiaryColors, textColors, trainingData } from "./constants.js"
 
@@ -6,91 +6,83 @@ const ai = new AI(trainingData).train()
 
 // console.log(ai);
 
-const primaryColorEl = document.getElementById("primary-color");
-const secondaryColorEl = document.getElementById("secondary-color");
-const tertiaryColorEl = document.getElementById("tertiary-color");
-const textColorEl = document.getElementById("text-color");
-const generateButton = document.getElementById("generate-btn");
-const saveButton = document.getElementById("save-btn");
+const primaryColorEl = document.getElementById("primary-color")
+const secondaryColorEl = document.getElementById("secondary-color")
+const tertiaryColorEl = document.getElementById("tertiary-color")
+const textColorEl = document.getElementById("text-color")
+const generateButton = document.getElementById("generate-btn")
+const saveButton = document.getElementById("save-btn")
 
-let match;
+let match
 const tracked = []
 const saved = []
 
 const generate = () => {
-  const redShade = convertRGB(
-    `255, ${randomInt()}, ${randomInt()}`,
-    true
-  );
-  const blueShade = convertRGB(
-    `${randomInt()}, ${randomInt()}, 255`,
-    true
-  );
+  const redShade = convertRGB(`255, ${randomInt()}, ${randomInt()}`, true)
+  const blueShade = convertRGB(`${randomInt()}, ${randomInt()}, 255`, true)
 
-  const tertiaryColor = convertRGB(shuffle(tertiaryColors)[0], true);
-  const textColor = convertRGB(shuffle(textColors)[0], true);
+  const tertiaryColor = convertRGB(shuffle(tertiaryColors)[0], true)
+  const textColor = convertRGB(shuffle(textColors)[0], true)
 
-  const [primaryColor, secondaryColor] = shuffle([redShade, blueShade]);
+  const [primaryColor, secondaryColor] = shuffle([redShade, blueShade])
 
   match = [primaryColor, secondaryColor, tertiaryColor, textColor].flat()
 
-  
-  if(match.length !== 12) {
+  if (match.length !== 12) {
     return generate()
   }
-  const guess = ai.net.run(match)[0];
+  const guess = ai.net.run(match)[0]
 
   console.log("guess ", guess)
 
-  if (guess > 0.5) {
-		primaryColorEl.style.backgroundColor = `rgba(${convertRGB(
-			primaryColor,
-			false,
-		).toString()})`
+  if (guess > 0.7) {
+    primaryColorEl.style.backgroundColor = `rgba(${convertRGB(
+      primaryColor,
+      false,
+    ).toString()})`
 
-		secondaryColorEl.style.backgroundColor = `rgba(${convertRGB(
-			secondaryColor,
-			false,
-		).toString()})`
+    secondaryColorEl.style.backgroundColor = `rgba(${convertRGB(
+      secondaryColor,
+      false,
+    ).toString()})`
 
-		tertiaryColorEl.style.backgroundColor = `rgba(${convertRGB(
-			tertiaryColor,
-			false,
-		).toString()})`
+    tertiaryColorEl.style.backgroundColor = `rgba(${convertRGB(
+      tertiaryColor,
+      false,
+    ).toString()})`
 
-		textColorEl.style.backgroundColor = `rgba(${convertRGB(
-			textColor,
-			false,
-		).toString()})`
+    textColorEl.style.backgroundColor = `rgba(${convertRGB(
+      textColor,
+      false,
+    ).toString()})`
 
-		tracked.push({
-			input: match,
-			output: [1],
-		})
+    tracked.push({
+      input: match,
+      output: [1],
+    })
 
-		return
-	}
+    return
+  }
 
-  generate();
-};
+  generate()
+}
 
-generateButton.addEventListener("click", generate);
+generateButton.addEventListener("click", generate)
 
 saveButton.addEventListener("click", () => {
-  // Remove and modify last item in stack 
-  const current = tracked.pop();
+  // Remove and modify last item in stack
+  const current = tracked.pop()
 
   // push match to trainingData
   saved.push({
-		input: current.input,
-		output: [1],
-	})
+    input: current.input,
+    output: [1],
+  })
 
   ai.train([...ai.trainingData, ...saved])
 
+  console.log("tracked Data ", tracked)
+  console.log("saved Data ", saved)
+})
 
-  console.log("tracked Data ", tracked);
-  console.log("saved Data ", saved);
-});
-
-generate();
+generate()
